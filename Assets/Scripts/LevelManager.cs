1e2c0fbbe92213e7812455a8fe40de9afe1d8bcb
Jaludi <mtj_000@hotmail.com>
2017-01-05 16:05:37 -0500

    0.0.3
    added scene loader, menu scene, game over and controls

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    int currentLevel;
+    // Use this for initialization
+    void Start () {
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameLoader();
+        }
+	}
+
+    void gameLoader()
+    {
+        if (currentLevel == 2)
+            currentLevel = -1;
+        SceneManager.LoadScene(currentLevel + 1);
+
+    }
+}
